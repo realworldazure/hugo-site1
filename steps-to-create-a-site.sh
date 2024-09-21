a78224c248eb092520ccfa441a1128f9514f30ce
Darren Robinson <mrdarrenjrobinsontech@gmail.com>
2024-09-21 21:49:30 +0100

    added some steps to create a site

---
+++ steps-to-create-a-site.sh
@@ -0,0 +1,7 @@
+Step	Command / task	            Comment
+1	    Install homebrew	        Go and get homebrew, if you haven't got it  already?
+2	    brew install hugo	        Install hugo
+3	    which hugo                  Validate that Hugo has been installed, you should get the   path of Hugo returned
+4	    hugo version	            Returns the version of Hugo installed
+5	    Create and clone a repo	    This is for your site
+6	    Hugo new site hugo-site1	Go into your repo and create your site
